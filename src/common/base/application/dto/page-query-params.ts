e279ebe225f6cd9e913f39c50c5340ac2c0ebbac
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-05-09 13:00:55 -0300

    Add query params DTOs with their interfaces

---
+++ src/common/base/application/dto/page-query-params.ts
@@ -0,0 +1,21 @@
+import { IsInt, Max, Min } from 'class-validator';
+
+import {
+  DEFAULT_PAGE_NUMBER,
+  DEFAULT_PAGE_SIZE,
+} from '@common/base/application/constant/base.constants';
+
+export class PageQueryParamsDto {
+  @IsInt()
+  @Min(1)
+  number?: number = DEFAULT_PAGE_NUMBER;
+
+  @IsInt()
+  @Min(1)
+  @Max(50)
+  size?: number = DEFAULT_PAGE_SIZE;
+
+  get offset(): number {
+    return (this.number - 1) * this.size;
+  }
+}
