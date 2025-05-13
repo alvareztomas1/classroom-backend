3a6b2e2021270133ab5e59a2d72a4727322e6a1f
Tomás Alvarez <47418131+alvareztomas1@users.noreply.github.com>
2025-05-13 14:24:45 -0300

    feat: Add local cognito setup (#8)
    * Install cognito SDK for creating CognitoService
    
    * Add config for using cognito local
    
    * Add missing mapped types nestjs dependency

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -4,6 +4,12 @@ export const environmentConfig = (): Record<string, any> => ({
     port: Number(process.env.PORT),
     baseUrl: process.env.BASE_APP_URL,
   },
+  cognito: {
+    userPoolId: process.env.COGNITO_USER_POOL_ID,
+    clientId: process.env.COGNITO_CLIENT_ID,
+    issuer: process.env.COGNITO_ISSUER,
+    endpoint: process.env.COGNITO_ENDPOINT,
+  },
   frontend: {
     url: process.env.FRONTEND_URL,
   },
