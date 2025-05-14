a866e3ea69849fbf56ba6f45cbf0864a9a80a218
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-05-14 14:53:31 -0300

    Add missing ISerializeResponseDtoParams interface

--- src/common/base/application/dto/serialized-response.interface.ts
+++ src/common/base/application/dto/serialized-response.interface.ts
@@ -1,4 +1,5 @@
 import { IPagingCollectionData } from '@common/base/application/dto/collection.interface';
+import { IDto } from '@common/base/application/dto/dto.interface';
 import { HttpMethod } from '@common/base/application/enum/http-method.enum';
 
 export interface ILink {
@@ -31,4 +32,11 @@ export interface ISerializedResponseData<ResponseDto extends IDto> {
   type: string;
   id?: string;
   attributes: ResponseDto;
+}
+export interface ISerializeResponseDtoParams<ResponseDto extends IDto> {
+  responseDto: ResponseDto;
+  entityName: string;
+  id?: string;
+  hasUpdate?: boolean;
+  hasDelete?: boolean;
 }
