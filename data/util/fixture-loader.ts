b23dc905e0c316a56a279871b315fcb28919f724
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-05-19 15:52:23 -0300

    Remove unnecessary eslint disable options

--- data/util/fixture-loader.ts
+++ data/util/fixture-loader.ts
@@ -1,8 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { resolve } from 'path';
 import {
   DataSource,
