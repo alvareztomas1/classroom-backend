2d0a1b678dfd7da2c4fd822bde08d5133e74a128
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-05-21 14:39:25 -0300

    Add updateMe mechanism to UserModule

--- src/module/iam/user/application/dto/update-user.dto.ts
+++ src/module/iam/user/application/dto/update-user.dto.ts
@@ -1,5 +1,25 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional, IsString } from 'class-validator';
 
 import { UserDto } from '@module/iam/user/application/dto/user.dto';
 
-export class UpdateUserDto extends PartialType(UserDto) {}
+export class UpdateUserDto
+  implements Pick<UserDto, 'firstName' | 'lastName' | 'avatarUrl'>
+{
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+
+  @IsString()
+  @IsOptional()
+  avatarUrl?: string;
+
+  constructor(firstName?: string, lastName?: string, avatarUrl?: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.avatarUrl = avatarUrl;
+  }
+}
--- src/module/iam/user/application/service/user.service.ts
+++ src/module/iam/user/application/service/user.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CollectionDto } from '@common/base/application/dto/collection.dto';
 import { IGetAllOptions } from '@common/base/application/dto/get-all-options.interface';
 
+import { UpdateUserDto } from '@module/iam/user/application/dto/update-user.dto';
 import { UserResponseDto } from '@module/iam/user/application/dto/user-response.dto';
 import { UserDto } from '@module/iam/user/application/dto/user.dto';
 import { UserMapper } from '@module/iam/user/application/mapper/user.mapper';
@@ -36,5 +37,18 @@ export class UserService {
 
   getMe(user: User): UserResponseDto {
     return this.mapper.fromEntityToResponseDto(user);
+  }
+
+  async updateMe(
+    user: User,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseDto> {
+    const userToUpdate = this.mapper.fromUpdateDtoToEntity(updateUserDto, user);
+    const updatedUser = await this.repository.updateOneOrFail(
+      user.id,
+      userToUpdate,
+    );
+
+    return this.mapper.fromEntityToResponseDto(updatedUser);
   }
 }
