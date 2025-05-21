7ef08b22551de9937c58dd965cf8eaa820b4c9a0
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-05-21 12:35:01 -0300

    Add AuthorizationModule with root config for storing policy handlers and feature config to define specific handlers

--- src/module/iam/iam.module.ts
+++ src/module/iam/iam.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { AuthenticationModule } from '@module/iam/authentication/authentication.module';
+import { AuthorizationModule } from '@module/iam/authorization/authorization.module';
 import { UserModule } from '@module/iam/user/user.module';
 
 @Module({
-  imports: [UserModule, AuthenticationModule],
+  imports: [UserModule, AuthenticationModule, AuthorizationModule.forRoot()],
 })
 export class IamModule {}
