21011c89b9646411aef5d02d17dc8f7779104cdd
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-05-23 11:27:22 -0300

    Update auth tests with avatar saving tests

--- src/test/test.module.bootstrapper.ts
+++ src/test/test.module.bootstrapper.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppModule } from '@module/app.module';
+import { IMAGE_STORAGE_PROVIDER_SERVICE_KEY } from '@module/cloud/application/interface/image-storage-provider.interface';
 import { IDENTITY_PROVIDER_SERVICE_KEY } from '@module/iam/authentication/application/service/identity-provider.service.interface';
 
 export const identityProviderServiceMock = {
@@ -13,11 +14,17 @@ export const identityProviderServiceMock = {
   refreshSession: jest.fn(),
 };
 
+export const imageStorageProviderServiceMock = {
+  uploadFile: jest.fn(() => Promise.resolve('test-url')),
+};
+
 export const testModuleBootstrapper = (): Promise<TestingModule> => {
   return Test.createTestingModule({
     imports: [AppModule],
   })
     .overrideProvider(IDENTITY_PROVIDER_SERVICE_KEY)
     .useValue(identityProviderServiceMock)
+    .overrideProvider(IMAGE_STORAGE_PROVIDER_SERVICE_KEY)
+    .useValue(imageStorageProviderServiceMock)
     .compile();
 };
