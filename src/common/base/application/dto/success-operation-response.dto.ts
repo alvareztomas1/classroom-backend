67702f17d35be348cb28144412f60caed80a3991
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-05-26 15:21:07 -0300

    Refactor base delete method from BaseCRUDService with SuccessOperationResponseDto

--- src/common/base/application/dto/success-operation-response.dto.ts
+++ src/common/base/application/dto/success-operation-response.dto.ts
@@ -1,6 +1,8 @@
 import { BaseResponseDto } from '@common/base/application/dto/base.response.dto';
 import { ISuccessfulOperationResponse } from '@common/base/application/dto/successful-operation-response.interface';
 
+export const OPERATION_RESPONSE_TYPE = 'operation';
+
 export class SuccessOperationResponseDto
   extends BaseResponseDto
   implements ISuccessfulOperationResponse
