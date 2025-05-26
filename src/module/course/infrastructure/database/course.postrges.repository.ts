b72ce0474d60b12fd0803ba4f4e42f700b95a4b8
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-05-26 18:40:02 -0300

    Add logic for saving courses with unique slugs

--- src/module/course/infrastructure/database/course.postrges.repository.ts
+++ src/module/course/infrastructure/database/course.postrges.repository.ts
@@ -4,12 +4,26 @@ import { Repository } from 'typeorm';
 
 import BaseRepository from '@common/base/infrastructure/database/base.repository';
 
+import { ICourseRepository } from '@module/course/application/repository/repository.interface';
 import { Course } from '@module/course/domain/course.entity';
 import { CourseSchema } from '@module/course/infrastructure/database/course.schema';
 
 @Injectable()
-export class CoursePostgresRepository extends BaseRepository<Course> {
+export class CoursePostgresRepository
+  extends BaseRepository<Course>
+  implements ICourseRepository
+{
   constructor(@InjectRepository(CourseSchema) repository: Repository<Course>) {
     super(repository);
+  }
+
+  async getSlugsStartingWith(slug: string): Promise<string[]> {
+    const results: { course_slug: string }[] = await this.repository
+      .createQueryBuilder('course')
+      .select('course.slug')
+      .where('course.slug LIKE :slug', { slug: `${slug}%` })
+      .getRawMany();
+
+    return results.map((r) => r.course_slug);
   }
 }
