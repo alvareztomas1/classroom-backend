3724332b790de33c65151f58bb8e7d1511f06a21
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-06-11 10:34:59 -0300

    Add Section Controller with patch and delete routes

---
+++ src/module/section/interface/section.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+} from '@nestjs/common';
+
+import { Hypermedia } from '@common/base/application/decorator/hypermedia.decorator';
+import { SuccessOperationResponseDto } from '@common/base/application/dto/success-operation-response.dto';
+import { HttpMethod } from '@common/base/application/enum/http-method.enum';
+
+import { SectionResponseDto } from '@module/section/application/dto/section.response.dto';
+import { UpdateSectionDto } from '@module/section/application/dto/update.section.dto';
+import { SectionService } from '@module/section/application/service/section.service';
+
+@Controller('section')
+export class SectionController {
+  constructor(private readonly sectionService: SectionService) {}
+
+  @Patch(':id')
+  @Hypermedia([
+    {
+      endpoint: '/course/:courseId/section',
+      method: HttpMethod.POST,
+      rel: 'create-section',
+    },
+    {
+      endpoint: '/section/:id',
+      method: HttpMethod.DELETE,
+      rel: 'delete-section',
+    },
+  ])
+  async updateOne(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateDto: UpdateSectionDto,
+  ): Promise<SectionResponseDto> {
+    return this.sectionService.updateOne(id, updateDto);
+  }
+
+  @Delete(':id')
+  async deleteOneByIdOrFail(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<SuccessOperationResponseDto> {
+    return this.sectionService.deleteOneByIdOrFail(id);
+  }
+}
