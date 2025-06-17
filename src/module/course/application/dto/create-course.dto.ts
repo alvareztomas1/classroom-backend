432595569a394e559cab4a1e69b60cd585df0186
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-06-17 16:14:25 -0300

    Refactor CreateCrouseDto to omit imageUrl on request's query params

--- src/module/course/application/dto/create-course.dto.ts
+++ src/module/course/application/dto/create-course.dto.ts
@@ -4,6 +4,7 @@ import { CourseDto } from '@module/course/application/dto/course.dto';
 
 export class CreateCourseRequestDto extends OmitType(CourseDto, [
   'instructorId',
+  'imageUrl',
 ]) {}
 
 export class CreateCourseDto extends CourseDto {}
