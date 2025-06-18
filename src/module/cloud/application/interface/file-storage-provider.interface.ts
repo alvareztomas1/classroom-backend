efe38dffbb8dc058502f275a882dae3619f6e283
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-06-18 11:51:30 -0300

    Change ImageStorageService name into FileStorageService

---
+++ src/module/cloud/application/interface/file-storage-provider.interface.ts
@@ -0,0 +1,6 @@
+export const FILE_STORAGE_PROVIDER_SERVICE_KEY =
+  'file_storage_provider_service';
+
+export interface IFileStorageProvider {
+  uploadFile(file: Express.Multer.File, folder: string): Promise<string>;
+}
--- src/module/cloud/application/interface/image-storage-provider.interface.ts
+++
@@ -1,6 +0,0 @@
-export const IMAGE_STORAGE_PROVIDER_SERVICE_KEY =
-  'image_storage_provider_service';
-
-export interface IImageStorageProvider {
-  uploadFile(file: Express.Multer.File, folder: string): Promise<string>;
-}
