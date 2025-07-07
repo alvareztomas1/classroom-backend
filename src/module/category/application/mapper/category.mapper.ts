122c4c63c756917c0af191db25dcb317e86fc578
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-07-07 17:43:22 -0300

    Refactor Category ResponseDto to include category's path to its tree's ancestors

--- src/module/category/application/mapper/category.mapper.ts
+++ src/module/category/application/mapper/category.mapper.ts
@@ -3,15 +3,19 @@ import { IDtoMapper } from '@common/base/application/dto/dto.interface';
 import { CategoryResponseDto } from '@module/category/application/dto/category-response.dto';
 import { CreateCategoryDto } from '@module/category/application/dto/create-category.dto';
 import { UpdateCategoryDto } from '@module/category/application/dto/update-category.dto';
+import { CategoryWithAncestors } from '@module/category/application/repository/category.repository.interface';
 import { Category } from '@module/category/domain/category.entity';
 
 export class CategoryMapper
   implements
-    IDtoMapper<
-      Category,
-      CreateCategoryDto,
-      UpdateCategoryDto,
-      CategoryResponseDto
+    Omit<
+      IDtoMapper<
+        Category,
+        CreateCategoryDto,
+        UpdateCategoryDto,
+        CategoryResponseDto
+      >,
+      'fromEntityToResponseDto'
     >
 {
   fromCreateDtoToEntity(dto: CreateCategoryDto): Category {
@@ -26,14 +30,14 @@ export class CategoryMapper
     return new Category(dto.name ?? entity.name, id, parent, subCategories);
   }
 
-  fromEntityToResponseDto(entity: Category): CategoryResponseDto {
-    const { name, id, parent, subCategories } = entity;
+  fromEntityToResponseDto(entity: CategoryWithAncestors): CategoryResponseDto {
+    const { name, id, ancestors } = entity;
+
     return new CategoryResponseDto(
       Category.getEntityName(),
       name,
       id,
-      parent,
-      subCategories,
+      ancestors,
     );
   }
 }
