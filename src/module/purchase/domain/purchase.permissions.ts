e94ce5351d897b13e8a4dd3fe8a8dfc96126922f
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-07-31 10:30:27 -0300

    Add purchase permissions definition

---
+++ src/module/purchase/domain/purchase.permissions.ts
@@ -0,0 +1,20 @@
+import { AppRole } from '@module/iam/authorization/domain/app-role.enum';
+import { AppAction } from '@module/iam/authorization/domain/app.action.enum';
+import { IPermissionsDefinition } from '@module/iam/authorization/infrastructure/policy/type/permissions-definition.interface';
+import { Purchase } from '@module/purchase/domain/purchase.entity';
+
+export const purchasePermissions: IPermissionsDefinition = {
+  [AppRole.Regular](user, { can }) {
+    can(AppAction.Read, Purchase, {
+      userId: user.id,
+    });
+  },
+  [AppRole.Admin](user, { can }) {
+    can(AppAction.Read, Purchase, {
+      userId: user.id,
+    });
+  },
+  [AppRole.SuperAdmin](_, { can }) {
+    can(AppAction.Manage, Purchase);
+  },
+};
