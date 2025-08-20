6268dd14f3c26990d18ed1dede7fec8b899e75e6
Tomás Alvarez <alvareztomas1@hotmail.com>
2025-08-20 10:53:41 -0300

    Fix purchase status transitions with pending -> cancelled

--- src/module/purchase/application/service/purchase-CRUD.service.ts
+++ src/module/purchase/application/service/purchase-CRUD.service.ts
@@ -179,6 +179,7 @@ export class PurchaseCRUDService
       [PurchaseStatus.PENDING]: [
         PurchaseStatus.COMPLETED,
         PurchaseStatus.FAILED,
+        PurchaseStatus.CANCELLED,
       ],
       [PurchaseStatus.COMPLETED]: [PurchaseStatus.REFUNDED],
       [PurchaseStatus.FAILED]: [],
